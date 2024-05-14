13c1f2fa8b91477a842f2fd52ba8dac856c9aba8
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-05-14 10:47:07 +0800

    v2_基本觸控手勢偵測

--- app/src/main/java/tw/edu/pu/csim/tcyang/gesture/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/gesture/MainActivity.kt
@@ -3,12 +3,21 @@ package tw.edu.pu.csim.tcyang.gesture
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.tcyang.gesture.ui.theme.GestureTheme
 
@@ -22,7 +31,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    Tap()
                 }
             }
         }
@@ -30,17 +40,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Tap() {
+    var msg by remember { mutableStateOf("TAP相關手勢實例") }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GestureTheme {
-        Greeting("Android")
+    Column {
+        Text(text = msg)
+
+        Image(
+            painter = painterResource(id = R.drawable.pu0),
+            contentDescription = "靜宜之美",
+            modifier = Modifier
+                .fillMaxSize()
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onTap = { msg = "後觸發onTap(短按)" },
+                        onDoubleTap = { msg = "雙擊" },
+                        onLongPress = { msg = "長按" },
+                        onPress = { msg = "先觸發onPress(按下)" }
+
+                    )
+                }
+        )
     }
 }
